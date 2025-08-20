e6f8cf61c4ca0988c68e2ef7f383f5ed05573b24
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-08-20 14:39:43 +0800

    added sign up page

---
+++ GreenBill/MVVM/ViewModel/SignupViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GreenBill.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenBill.MVVM.ViewModel
+{
+    public class SignupViewModel : Core.ViewModel
+    {
+        private INavigationService _navigationService;
+
+        public INavigationService Navigation
+        {
+            get => _navigationService;
+            set
+            {
+                _navigationService = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SignupViewModel() { }
+
+        public SignupViewModel(INavigationService navService)
+        {
+            Navigation = navService;
+        }
+    }
+}

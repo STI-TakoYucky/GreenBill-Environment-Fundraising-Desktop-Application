98218b0b15ee9f9fd9b10764990d46eac0a6e11b
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-08-20 10:52:37 +0800

    added services

--- GreenBill/App.xaml.cs
+++ GreenBill/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GreenBill.MVVM.ViewModel;
+using GreenBill.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,29 @@ namespace GreenBill
     /// </summary>
     public partial class App : Application
     {
+           private readonly IServiceProvider serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainWindowViewModel>()
+            });
+
+            services.AddSingleton<MainWindowViewModel>();
+
+            services.AddSingleton<INavigationService, NavigationService>();
+            services.AddSingleton<Func<Type, Core.ViewModel>>(ServiceProvider => viewModelType => (Core.ViewModel)ServiceProvider.GetRequiredService(viewModelType));
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }

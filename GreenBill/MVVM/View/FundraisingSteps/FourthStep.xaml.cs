17a36e526c71e2e196a9b71980a59ac7747373fd
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-08-21 22:01:29 +0800

    added fundraising steps pages

---
+++ GreenBill/MVVM/View/FundraisingSteps/FourthStep.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GreenBill.MVVM.View.FundraisingSteps
+{
+    /// <summary>
+    /// Interaction logic for FourthStep.xaml
+    /// </summary>
+    public partial class FourthStep : UserControl
+    {
+        public FourthStep()
+        {
+            InitializeComponent();
+        }
+    }
+}

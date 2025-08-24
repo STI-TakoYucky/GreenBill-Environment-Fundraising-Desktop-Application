1ab716f020da5616d0cd129313d3e59777a4d298
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-08-24 12:00:38 +0800

    Add ImageConverter

---
+++ GreenBill/Converters/ByteArrayToImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace GreenBill.Converters
+{
+    public class ByteArrayToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bytes = value as byte[];
+            if (bytes == null || bytes.Length == 0) return null;
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                var bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.CacheOption = BitmapCacheOption.OnLoad;
+                bmp.StreamSource = ms;
+                bmp.EndInit();
+                bmp.Freeze();
+                return bmp;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

2ada5e6fa1c07ebdc2250f4b8f8cfc13debbfd6e
STI-TakoYucky <estrada.lnp@gmail.com>
2025-09-04 01:25:04 +0800

    created an admin window to dynamically switch through tabs

--- GreenBill/MVVM/View/Admin/AdminDashboard.xaml.cs
+++ GreenBill/MVVM/View/Admin/AdminDashboard.xaml.cs
@@ -1,5 +1,4 @@
-﻿using GreenBill.MVVM.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +16,11 @@ using System.Windows.Shapes;
 namespace GreenBill
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AdminDashboard.xaml/
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdminDashboard : Window
     {
-        public MainWindow()
+        public AdminDashboard()
         {
             InitializeComponent();
         }

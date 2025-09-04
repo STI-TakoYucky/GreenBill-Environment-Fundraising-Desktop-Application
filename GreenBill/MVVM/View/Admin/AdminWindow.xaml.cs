6c7b39ae39c61cf36558dacac7bc45757558fd75
STI-TakoYucky <estrada.lnp@gmail.com>
2025-09-04 12:49:15 +0800

    fixed merge conflicts

--- GreenBill/MVVM/View/Admin/AdminWindow.xaml.cs
+++ GreenBill/MVVM/View/Admin/AdminWindow.xaml.cs
@@ -19,26 +19,17 @@ namespace GreenBill
     /// <summary>
     /// Interaction logic for AdminDashboard.xaml
     /// </summary>
-<<<<<<< HEAD
     public partial class AdminWindow : Window
     {
         public AdminWindow()
-=======
-    public partial class AdminDashboard : Window
-    {
-        public AdminDashboard()
->>>>>>> 3c78753372bd1e2fd5f9825f5267bfa36b3d0956
         {
             InitializeComponent();
             DataContext = new AdminDashboardViewModel();
         }
-<<<<<<< HEAD
 
         private void InitializeComponent()
         {
             throw new NotImplementedException();
         }
-=======
->>>>>>> 3c78753372bd1e2fd5f9825f5267bfa36b3d0956
     }
 }
--- GreenBill/MVVM/ViewModel/Admin/AdminWindowViewModel.cs
+++ GreenBill/MVVM/ViewModel/Admin/AdminWindowViewModel.cs
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-﻿using System;
-=======
-﻿using LiveChartsCore;
-using LiveChartsCore.Defaults;
-using LiveChartsCore.Kernel.Sketches;
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
-using LiveChartsCore.SkiaSharpView.Painting;
-using LiveChartsCore.SkiaSharpView.VisualElements;
-using SkiaSharp;
+
 using System;
->>>>>>> 3c78753372bd1e2fd5f9825f5267bfa36b3d0956
 
 namespace GreenBill
 {

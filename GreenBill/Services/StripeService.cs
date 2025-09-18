e62bd55d792a43e96d3e775bdbdec38bf58d0885
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-09-17 23:20:16 -0700

    Fixed the github protection issue

---
+++ GreenBill/Services/StripeService.cs
@@ -0,0 +1,18 @@
+﻿using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenBill.Services
+{
+    public class StripeService
+    {
+        public StripeService() 
+        {
+            StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("STRIPE_API_KEY");
+        }
+
+    }
+}

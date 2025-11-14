f921271f43e90844eb1b43864878a945868c32d0
STI-TakoYucky <estrada.lnp@gmail.com>
2025-11-14 21:34:48 +0800

    Added preview image to the user profile

--- GreenBill/MVVM/ViewModel/Admin/ReviewUserViewModel.cs
+++ GreenBill/MVVM/ViewModel/Admin/ReviewUserViewModel.cs
@@ -4,11 +4,15 @@ using GreenBill.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GreenBill.MVVM.ViewModel.Admin {
     internal class ReviewUserViewModel : Core.ViewModel, INavigatableService {
@@ -25,11 +29,13 @@ namespace GreenBill.MVVM.ViewModel.Admin {
         public IUserService _userService;
 
         public ICommand NavigateBack { get; set; }
+        public ICommand PreviewImageCommand { get; }
         public ReviewUserViewModel(ITabNavigationService navigationService, IUserService userService) {
             Navigation = navigationService;
 
             NavigateBack = new RelayCommand(Navigation.NavigateToTab<UserAnalyticsViewModel>);
             _userService = userService;
+            PreviewImageCommand = new RelayCommand(param => PreviewImage(param));
         }
 
         private GreenBill.MVVM.Model.User _selectedUser;
@@ -38,6 +44,31 @@ namespace GreenBill.MVVM.ViewModel.Admin {
             set {
                 _selectedUser = value;
                 OnPropertyChanged();
+            }
+        }
+
+        public void PreviewImage(object parameter) {
+            if (parameter is byte[] imageBytes) {
+                // Convert byte array to ImageSource
+                var image = new BitmapImage();
+                using (var stream = new MemoryStream(imageBytes)) {
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.StreamSource = stream;
+                    image.EndInit();
+                    image.Freeze(); // optional, makes it cross-thread safe
+                }
+
+                var window = new Window {
+                    Title = SelectedUser?.Username ?? "Preview",
+                    Width = 800,
+                    Height = 600,
+                    Content = new Image {
+                        Source = image,
+                        Stretch = Stretch.Uniform
+                    }
+                };
+                window.ShowDialog();
             }
         }
 
